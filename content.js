78c734fa2fdce57d9f6d07837822720804423ec2
hungtran97 <hungtran.nh97@gmail.com>
2017-12-06 04:35:52 +0700

    link to github

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 if(window.location.hostname==='wake.unixcoin.com'){
 	var s = document.createElement("script");
     s.type = "text/javascript";
-    s.src = "https://raw.githubusercontent.com/huqcan9x/mini/master/unixcoin.js";
+    s.src = "https:///cdn.rawgit.com/huqcan9x/mini/master/unixcoin.js";
     $("body").append(s);
 }
